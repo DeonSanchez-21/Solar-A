fab61cb344551d6e0897442505b6bc10b5485c10
Deon <dsanchez6@gmail.com>
2022-07-30 13:04:04 -0400

    first

---
+++ js/contact.js
@@ -0,0 +1,17 @@
+
+
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "deonsanchez6@gmail.com",
+        Password : "706EFF27EBCFD328815416D07F52318008B5",
+        To : 'amohammad@momentumsolar.com',
+        From : document.getElementById("email").value,
+        Subject : "new contact form inquery",
+        Body : `Name: ${document.getElementById("first-name").value} ${document.getElementById("last-name").value}<br>
+        Number: ${document.getElementById("phone-number").value}<br>
+        Message: ${document.getElementById("message").value}`,
+    }).then(message => alert(message)
+    );
+} 
